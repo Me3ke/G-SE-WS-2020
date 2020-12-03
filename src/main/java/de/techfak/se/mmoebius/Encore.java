1d4289c43fb410a4a812b65cbcdb922f6e311073
Maiko Möbius <mmoebius@techfak.de>
2020-12-03 18:11:50 +0100

    added program argument query. closes #19789 @1.1h

--- src/main/java/de/techfak/se/mmoebius/Encore.java
+++ src/main/java/de/techfak/se/mmoebius/Encore.java
@@ -1,5 +1,11 @@
 package de.techfak.se.mmoebius;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * The main class, contains just the main method to start the application.
  */
@@ -9,8 +15,20 @@ public final class Encore {
     }
 
     public static void main(final String... args) {
-        Game g = new Game();
-        g.play();
+        Game game = new Game();
+        game.play();
+        if(args.length == 0) {
+            System.out.println("No programarguments given");
+            System.exit(100);
+        }
+        else if(args[0].equals("-f")){
+            //hier überprüfen ob datei korrekt
+            File file = new File(args[1]);
+        }
+        else{
+            System.out.println("unknown programargument");
+            System.exit(100);
+        }
     }
 
 }
